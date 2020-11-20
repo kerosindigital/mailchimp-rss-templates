58a18131dc569b03de6c33a0887a25cab24dfc0e
das-gernot <53981883+das-gernot@users.noreply.github.com>
2020-11-20 20:42:52 +0100

    Create rss-chimp-template_image-left.php

---
+++ rss-chimp-template_image-left.php
@@ -0,0 +1,39 @@
+<table border="0" cellpadding="0" cellspacing="0" width="100%" class="mcnCaptionBlock" style="border-collapse: collapse;mso-table-lspace: 0pt;mso-table-rspace: 0pt;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;">
+    <tbody class="mcnCaptionBlockOuter">
+        <tr>
+			<td class="mcnCaptionBlockInner" valign="top" style="padding: 9px;mso-line-height-rule: exactly;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;">
+
+            	*|FEEDBLOCK:https://kerosin.digital/feed|* *|FEEDITEMS:[$count=7]|*
+				
+				<table border="0" cellpadding="0" cellspacing="0" class="mcnCaptionRightContentOuter" width="100%" style="border-collapse: collapse;mso-table-lspace: 0pt;mso-table-rspace: 0pt;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;">
+					<tbody><tr>
+						<td valign="top" class="mcnCaptionRightContentInner" style="padding: 0 9px;mso-line-height-rule: exactly;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;">
+							
+							<table align="left" border="0" cellpadding="0" cellspacing="0" class="mcnCaptionRightImageContentContainer" width="176" style="border-collapse: collapse;mso-table-lspace: 0pt;mso-table-rspace: 0pt;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;">
+								<tbody><tr>
+								 <td class="mcnCaptionRightImageContent" align="left" valign="top" style="mso-line-height-rule: exactly;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;">          
+									<style>#rss-chimp-tl img {width: 100%;}</style>
+									<div id="rss-chimp-tl"><a href="*|RSSITEM:URL|*" target="_blank">*|FEEDITEM:IMAGE|*</a></div>
+								</td>
+								</tr></tbody>
+							</table>
+
+							<table class="mcnCaptionRightTextContentContainer" align="right" border="0" cellpadding="0" cellspacing="0" width="352" style="border-collapse: collapse;mso-table-lspace: 0pt;mso-table-rspace: 0pt;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;">
+								<tbody><tr>
+									<td valign="top" class="mcnTextContent" style="mso-line-height-rule: exactly;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;word-break: break-word;color: #757575;font-family: Helvetica;font-size: 16px;line-height: 150%;text-align: left;">
+										<h2 class="mc-toc-title"><a href="*|FEEDITEM:URL|*" target="_blank">*|FEEDITEM:TITLE|*</a></h2>
+										<p>*|FEEDITEM:CONTENT|*</p>
+									</td>
+								</tr></tbody>
+							</table>
+
+						</td>
+					</tr></tbody>
+				</table>
+
+				*|END:FEEDITEMS|* *|END:FEEDBLOCK|*
+
+            </td>
+        </tr>
+    </tbody>
+</table>
